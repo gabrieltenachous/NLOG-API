4ec9ae03df960c16383430d384372c4839306cd7
gabrieltenachous <gabriel_oliveira_f_sousa@outlook.com>
2020-12-15 10:21:47 -0300

    Comecando a web API, adicionando o NLOG

---
+++ LoggerService/LoggerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static Contracts.Contracts;
+using NLog;
+namespace LoggerService
+{
+    public class LoggerService : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarn(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
